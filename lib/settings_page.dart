504bea76509e153a3c2020efa123e73809bdb342
Nuri <nuriddin7612@mail.ru>
2022-06-15 13:42:25 +0900

    rate this app

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -30,7 +30,7 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('more     ')),
+        title: const Center(child: Text('Remove this page    ')),
       ),
       body: Container(),
       // body: Column(
