fe65094293dd2d6cc9c3dd25cb55c885af886028
Nuri <nuriddin7612@mail.ru>
2022-06-17 13:36:14 +0900

    SETTINGS

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -30,7 +30,7 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('settings   ')),
+        title: const Center(child: Text('MORE   ')),
       ),
       body: Container(),
       // body: Column(
