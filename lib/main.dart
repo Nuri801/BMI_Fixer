94ccadb3db7a604382affcc9b3f807ed6179a172
Nuri <nuriddin7612@mail.ru>
2022-07-25 01:56:09 +0900

    a new release

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         appBarTheme: AppBarTheme(
           backgroundColor: Color(0xFF0A0D22), //use your hex code here
